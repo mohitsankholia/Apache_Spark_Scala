7a0f03e00cad63902d5ca3fec76d28cecee4ae28
mohit <sankholia.mohit@gmail.com>
2023-01-13 16:09:35 +0530

    Apache Spark with Scala Big Data

---
+++ src/main/scala/scala/spark/HelloWorld.scala
@@ -0,0 +1,25 @@
+package scala.spark
+
+import org.apache.spark._
+import org.apache.log4j._
+
+/**
+ * @author Mohit Sankholia
+ *         on 09/01/23
+ */
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "HelloWorld")
+
+    val lines = sc.textFile("data/ml-100k/u.data")
+    val numLines = lines.count()
+
+    println("Hello world! The u.data file has " + numLines + " lines.")
+
+    sc.stop()
+  }
+}
---
+++ src/main/scala/scala/spark/WordCount.scala
@@ -0,0 +1,33 @@
+package scala.spark
+
+import org.apache.spark._
+import org.apache.log4j._
+
+/**
+ * @author Mohit Sankholia 
+ *         on 09/01/23
+ */
+object WordCount {
+
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "WordCount")
+
+    // Read each line of my book into an RDD
+    val input = sc.textFile("data/book.txt")
+
+    // Split into words separated by a space character
+    val words = input.flatMap(x => x.split(" "))
+
+    // Count up the occurrences of each word
+    val wordCounts = words.countByValue()
+
+    // Print the results.
+    wordCounts.foreach(println)
+  }
+
+}
